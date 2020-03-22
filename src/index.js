3ad2232ffce4e21dbef30341be7067bd3374259c
qlixxxy <sqlixor@gmail.com>
2020-03-22 23:05:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let letters = {};
+    let resultArray = expr.split(""); 
+    let letterLength = expr.length / 10;
+    for(let i = 0; i < letterLength; i++) {
+        let from = i*10;
+        let until = from + 10;
+        letters[i+1] = resultArray.slice(from, until).join("");
+    }
+    for(let i in letters) {
+        if (letters[i].indexOf(1) !== -1) {
+            let from = letters[i].indexOf(1);
+            letters[i] = letters[i].slice(from);
+        }
+    }
+    for(let i in letters) {
+        while(letters[i].includes('10')) {
+            letters[i] = letters[i].replace('10', '.');
+        }
+    }
+    for(let i in letters) {
+        while(letters[i].includes('11')) {
+            letters[i] = letters[i].replace('11', '-');
+        }
+    }
+    for(let i = 1; i <= letterLength; i++) {
+        if(letters[i] == '**********') {
+            result = result + ' ';
+        } else {
+            result = result + MORSE_TABLE[letters[i]];
+        }
+    }
+    return result;
 }
 
 module.exports = {
     decode
-}
+};
